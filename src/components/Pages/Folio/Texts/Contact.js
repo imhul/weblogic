b87f56e325e7599247ec0cfe2fdf60bc9d3fe5f6
Tkachuk Zakhar <blashirk@gmail.com>
2018-08-28 22:29:17 +0300

    works page in progress

--- src/components/Pages/Folio/Texts/Contact.js
+++ src/components/Pages/Folio/Texts/Contact.js
@@ -18,11 +18,44 @@ class Contact extends Component {
     render() {
         return (
             <div className="container">
-           
+                <h2 class="uk-text-center">Connect with us</h2>
+
+                <div class="uk-margin-bottom" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                    <ul class="uk-list">
+                        <li>
+                            <div class="uk-clearfix">
+                                <div class="uk-float-right">
+                                    <a href="" onclick="event.preventDefault()" id="copy-mail" class="uk-button uk-button-default uk-button-small center msgs"
+                                        data-message="<span uk-icon='icon: check'></span> Mail is copied ;)">Copy to clipboard</a>
+                                </div>
+                                <div class="uk-float-left">
+                                    <span id="decoded-mail" class="mobile-fix uk-margin-bottom uk-display-block uk-text-center"></span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="uk-clearfix">
+                                <div class="uk-float-right">
+                                    <a href="" onclick="event.preventDefault()" id="copy-phone" class="uk-button uk-button-default uk-button-small center msgs"
+                                        data-message="<span uk-icon='icon: check'></span> Phone is copied ;)">Copy to clipboard</a>
+                                </div>
+                                <div class="uk-float-left">
+                                    <span id="decoded-phone" class="mobile-fix uk-margin-bottom uk-display-block uk-text-center"></span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+                <p class="" itemprop="description">
+                    Development, design and creation of sites, SEO optimization, website promotion, as well as training courses on the creation
+                    and promotion of sites.
+                    <br />Разработка и создание сайтов, SEO оптимизация, раскрутка и продвижение сайтов, а так же обучающие курсы по
+                    созданию и продвижению сайтов.</p>
 
             </div>
-        )
-    }
-}
-
+                )
+            }
+        }
+        
 export default Contact
--- src/components/Pages/Folio/Texts/Works.js
+++ src/components/Pages/Folio/Texts/Works.js
@@ -17,7 +17,61 @@ class Works extends Component {
     render() {
         return (
             <div className="container">
-    
+            <h2 class="uk-accordion-title mobile-fix">
+            <span class="uk-margin-right" uk-icon="icon: cog"></span>What can we do?</h2>
+          <div class="uk-accordion-content">
+
+            <table class="uk-table uk-table-hover uk-table-responsive uk-table-justify uk-table-divider">
+              <thead>
+                <tr>
+                  <th>Project description</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Puzzle game, JavaScript</td>
+                  <td>
+                    {/* <a target="_blank" href="/src/lab/game/index.html" class="uk-button uk-button-default tm-text-uppercase">
+                          <span class="uk-margin-right" uk-icon="icon: sign-out"></span>Live demo
+                        </a> */}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Corporate project, CMS Joomla 3.6.2</td>
+                  <td>
+                    <a target="_blank" rel="nofollow" href="http://ekta.ua/" class="uk-button uk-button-default tm-text-uppercase">
+                      <span class="uk-margin-right" uk-icon="icon: sign-out"></span>Released
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>LED-screen calculator, Angular + Materialize</td>
+                  <td>
+                   {/* <a target="_blank" href="/src/lab/calc/index.html" class="uk-button uk-button-default">
+                          <span class="uk-margin-right" uk-icon="icon: sign-out"></span>Live demo
+                        </a>  */}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Analytical project, js + python + Materialize</td>
+                  <td>
+                    <a target="_blank" href="https://seezislab.com/" class="uk-button uk-button-default">
+                      <span class="uk-margin-right" uk-icon="icon: sign-out"></span>Released
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Web presentation, Rreveal.js</td>
+                  <td>
+                     {/* <a target="_blank" href="/src/lab/presentation/index.html" class="uk-button uk-button-default">
+                          <span class="uk-margin-right" uk-icon="icon: sign-out"></span>Live demo
+                        </a> */}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
             </div>
         )
