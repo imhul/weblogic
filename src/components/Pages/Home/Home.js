cbce9f041b0708253c18860e2aaa831ba090941d
Tkachuk Zakhar <blashirk@gmail.com>
2018-08-28 22:35:30 +0300

    home page: scss fixes needed

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -13,39 +13,38 @@ class Home extends Component {
     render() {
         return (
             <div className="Home">
-                <h1 className="mobile-fix heading-hero">
-                    <span className="span-0">W</span>
-                    <span className="span-1">e</span>
-                    <span className="span-2">b</span>
-                    <span className="span-3">L</span>
-                    <span className="span-4">o</span>
-                    <span className="span-5">g</span>
-                    <span className="span-6">i</span>
-                    <span className="span-7">c</span>
-                    <span className="span-8"> </span>
-                    <span className="span-9">S</span>
-                    <span className="span-10">t</span>
-                    <span className="span-11">u</span>
-                    <span className="span-12">d</span>
-                    <span className="span-13">i</span>
-                    <span className="span-14">o</span>
-                    <span className="span-15">
-                        <Link to="/folio">
+                <Link to="/folio">
+                    <h1 className="mobile-fix heading-hero">
+                        <span className="span-0">W</span>
+                        <span className="span-1">e</span>
+                        <span className="span-2">b</span>
+                        <span className="span-3">L</span>
+                        <span className="span-4">o</span>
+                        <span className="span-5">g</span>
+                        <span className="span-6">i</span>
+                        <span className="span-7">c</span>
+                        <span className="span-8"> </span>
+                        <span className="span-9">S</span>
+                        <span className="span-10">t</span>
+                        <span className="span-11">u</span>
+                        <span className="span-12">d</span>
+                        <span className="span-13">i</span>
+                        <span className="span-14">o</span>
+                        <span className="span-15">
                             <i className="icon-lamp"></i>
-                        </Link>
-                    </span>
-                    
-                    <div>
-                        <a
-                            href="https://github.com/imhul/weblogic"
-                            title="github project"
-                            className="github"
-                            target="_blank"
-                        >
-                            by Tkachuk Zakhar <Icon type="github" />
-                        </a>
-                    </div>
-                </h1>
+                        </span>
+                    </h1>
+                </Link>
+                <div>
+                    <a
+                        href="https://github.com/imhul/weblogic"
+                        title="github project"
+                        className="github"
+                        target="_blank"
+                    >
+                        by Tkachuk Zakhar <Icon type="github" />
+                    </a>
+                </div>
             </div>
         );
     }
