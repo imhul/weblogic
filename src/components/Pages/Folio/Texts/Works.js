3fd51d9baf7916f9f578585b81aecc433e6959c1
Tkachuk Zakhar <blashirk@gmail.com>
2018-09-03 23:00:26 +0300

    works content in progress

--- src/components/Pages/Folio/Texts/Works.js
+++ src/components/Pages/Folio/Texts/Works.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Row, Col, Button, Icon, message } from 'antd';
+import { Row, Col, List, Button, Icon, message } from 'antd';
 import { LanguageProvider, Language } from '../../../../utils/language/provider';
 
-const summarySuccess = () => {
-    message.info('Summary already downloaded');
-};
+const ListItem = List.Item;
+const ListMeta = ListItem.Meta;
 
 class Works extends Component {
     constructor(props) {
@@ -15,67 +14,90 @@ class Works extends Component {
     }
 
     render() {
-        return (
-            <div className="container">
-            <h2 className="uk-accordion-title mobile-fix">
-            <span className="uk-margin-right" uk-icon="icon: cog"></span>What can we do?</h2>
-          <div className="uk-accordion-content">
+        const links = [
+            {
+                name: 'Puzzle game, JavaScript',
+                type: 'Live demo',
+                href: '../../../../lab/game/index.html'
+            },
+            {
+                name: 'Corporate project, CMS Joomla 3.6.2',
+                type: 'Released',
+                href: 'http://ekta.ua/'
+            },
+            {
+                name: 'LED-screen calculator, Angular + Materialize',
+                type: 'Live demo',
+                href: '../../../../lab/calc/index.html'
+            },
+            {
+                name: 'Analytical project, js + python + Materialize',
+                type: 'Released',
+                href: 'https://seezislab.com/'
+            },
+            {
+                name: 'Web presentation, Rreveal.js',
+                type: 'Live demo',
+                href: '../../../../lab/presentation/index.html'
+            }
+        ];
 
-            <table className="uk-table uk-table-hover uk-table-responsive uk-table-justify uk-table-divider">
-              <thead>
-                <tr>
-                  <th>Project description</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Puzzle game, JavaScript</td>
-                  <td>
-                    {/* <a target="_blank" href="/src/lab/game/index.html" className="uk-button uk-button-default tm-text-uppercase">
-                          <span className="uk-margin-right" uk-icon="icon: sign-out"></span>Live demo
-                        </a> */}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Corporate project, CMS Joomla 3.6.2</td>
-                  <td>
-                    <a target="_blank" rel="nofollow" href="http://ekta.ua/" className="uk-button uk-button-default tm-text-uppercase">
-                      <span className="uk-margin-right" uk-icon="icon: sign-out"></span>Released
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>LED-screen calculator, Angular + Materialize</td>
-                  <td>
-                   {/* <a target="_blank" href="/src/lab/calc/index.html" className="uk-button uk-button-default">
-                          <span className="uk-margin-right" uk-icon="icon: sign-out"></span>Live demo
-                        </a>  */}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Analytical project, js + python + Materialize</td>
-                  <td>
-                    <a target="_blank" href="https://seezislab.com/" className="uk-button uk-button-default">
-                      <span className="uk-margin-right" uk-icon="icon: sign-out"></span>Released
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Web presentation, Rreveal.js</td>
-                  <td>
-                     {/* <a target="_blank" href="/src/lab/presentation/index.html" className="uk-button uk-button-default">
-                          <span className="uk-margin-right" uk-icon="icon: sign-out"></span>Live demo
-                        </a> */}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        return (
+            <div className="Works content">
+
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Language
+                            dictionary={{
+                                english: "Project description",
+                                russian: "Описание проекта"
+                            }}
+                        />
+                    </Col>
+                    <Col span={12}>
+                        <Language
+                            dictionary={{
+                                english: "Status",
+                                russian: "Статус"
+                            }}
+                        />
+                    </Col>
+                </Row>
+
+                <List
+                    size="large"
+                    header={<h2 className="mobile-fix">
+                        <Language
+                            dictionary={{
+                                english: "My works",
+                                russian: "Мои работы"
+                            }}
+                        />
+                    </h2>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={links}
+                    renderItem={item => (
+                        <ListItem>
+                            <a href={item.href}>
+                                {item.type}
+                                <Icon
+                                    type="link"
+                                    style={{
+                                        fontSize: '16px',
+                                        color: item.type === 'Released' ? '#fdd835' : '#bcc8ce'
+                                    }}
+                                />
+                            </a>
+
+                            <ListMeta title={item.name} />
+                        </ListItem>
+                    )}
+                />
 
             </div>
         )
     }
 }
 
-export default Works
+export default Works;
