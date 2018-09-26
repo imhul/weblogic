d7c6de737208f37b6c65a9dc6cef321aae0b28eb
Tkachuk Zakhar <blashirk@gmail.com>
2018-09-26 14:41:14 +0300

    package.json update

--- src/components/Pages/Folio/Texts/About.js
+++ src/components/Pages/Folio/Texts/About.js
@@ -18,30 +18,31 @@ class About extends Component {
 
     render() {
         return (
-            <div className="About">
+            // ant-timeline-alternate
+            <div className="About content">
                 <Timeline pending="2018: REDUX..." mode="alternate">
                     <TimelineItem color="green">
                         <span>Nov 2009: </span>
                         I started learning CSS, HTML, Joomla CMS and the web design basics with Photoshop. I made the first site on pure html and one more on Joomla</TimelineItem>
                     <TimelineItem>
-                        <span>2011: </span>
                         Got carried away by studying SEO
+                        <span> :2011</span>
                     </TimelineItem>
                     <TimelineItem>
                         <span>2013: </span>
                         Put my first record: brought the site from development to Google Search top-3 in three days
                     </TimelineItem>
                     <TimelineItem>
-                        <span>2015: </span>
                         By this time I have completed more than 50 individual orders for the site development based on Joomla CMS and decided to take courses in JavaScript
+                        <span> :2015</span>
                     </TimelineItem>
                     <TimelineItem>
                         <span>2016: </span>
                         There was a little experience with Angular. Learned the basics of Git, Jira, GitBash
                     </TimelineItem>
                     <TimelineItem>
-                        <span>Dec 2017: </span>
                         Started learning React
+                        <span> :Dec 2017</span>
                     </TimelineItem>
                 </Timeline>
             </div>
