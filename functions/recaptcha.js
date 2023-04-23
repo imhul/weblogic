98b5c95dc1689eba68359183a5d5f89bd3c8a77f
tkachuk <z.tkachuk@nodeart.io>
2023-04-23 10:48:17 +0300

    add functions

---
+++ functions/recaptcha.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+  try {
+    const response = await axios.post('https://www.google.com/recaptcha/api2/siteverify', {
+      secret: 'YOUR_SECRET_KEY',
+      response: event.body.response
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
+// У цьому коді ми створили функцію-обробник з іменем handler, яка отримує запит від веб-додатку, здійснює запит на Recaptcha API та повертає відповідь.
+
+// Збережіть файл recaptcha.js і перейдіть на сайт Netlify. Увійдіть в свій аккаунт та відкрийте свій проект.
+
+// Виберіть "Functions" з бічного меню та натисніть "Add function".
+
+// Введіть назву функції, наприклад "recaptcha" та виберіть "JavaScript" як мову.
+
+// Натисніть кнопку "Create Function" та скопіюйте код з файлу recaptcha.js в редактор функції.
+
+// Збережіть функцію та скопіюйте URL-адресу функції, який буде використовувати веб-додаток.
+
+// Ваш веб-додаток може здійснювати запит до функції з використанням Axios. Ось приклад коду:
+
+// axios.post('https://your-netlify-function-url.com/.netlify/functions/recaptcha', {
+//   response: 'USER_RESPONSE_TOKEN'
+// })
+// .then(response => {
+//   console.log(response.data);
+// })
+// .catch(error => {
+//   console.error(error);
+// });
+
+
+
+
+
+
+
