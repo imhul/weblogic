80e4229465fbed7eabdc3d76dc68159ea522b037
tkachuk <z.tkachuk@nodeart.io>
2023-05-05 02:41:00 +0300

    backend test 75

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,17 @@
 // netlify functions URLs
-const netlifyURL = 'https://weblogic.netlify.app/.netlify/functions/recaptcha';
+const netlifyURL = 'https://weblogic.netlify.app/.netlify/functions/recaptcha?data=';
 
 export const getRecaptcha = async data => {
     console.info('::: getRecaptcha :::');
 
     const config = {
-        method: 'POST',
+        method: 'GET',
         mode: 'no-cors', // no-cors, *cors, same-origin
         headers: { 'Content-Type': 'application/json', 'Allow': 'GET, POST, OPTIONS, HEAD' },
-        body: JSON.stringify({ data })
+        // body: JSON.stringify({ data })
     };
 
-    const result = await fetch(netlifyURL, config);
+    const result = await fetch(netlifyURL + '' + data, config);
     console.info('::: getRecaptcha result: ', result);
 
     if (result.ok) {
