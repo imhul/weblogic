0c604f85da90a74c3031142fe6f7b19483f32408
tkachuk <z.tkachuk@nodeart.io>
2023-05-05 16:28:01 +0300

    backend test 100

--- src/components/Pages/Folio/Texts/Captcha.jsx
+++ src/components/Pages/Folio/Texts/Captcha.jsx
@@ -1,81 +1,58 @@
 // core
 import React, { memo, useCallback, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 // components
 import { Row, Col } from 'antd/lib';
 import Recaptcha from 'react-recaptcha';
 // utils
-// import axios from 'axios';
 import safe from '../../../../utils/safe';
-// import translate from '../../../../utils/translations';
+import translate from '../../../../utils/translations';
 // hooks
 import useIpify from '../../../../hooks/useIpify';
-// import useRefresh from '../../../../hooks/useRefresh';
 import { getRecaptcha } from '../../../../utils/api';
 
 const Captcha = memo(() => {
     useIpify();
-    // const refreshed = useRefresh();
-    // const { currentUser } = useSelector(state => state.ui);
     const { lang } = useSelector(state => state.ux);
     const { currentUser } = useSelector(state => state.ui);
     const { key } = safe;
     const [ip, setIp] = useState('');
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-    // useEffect(() => console.info('ipified: ', ipified, 'refreshed: ', refreshed, 'id: ', currentUser.ip));
-
-    // const verify = useCallback(async data => {
-    //     if (!refreshed || !ipified || !currentUser.ip.length) {
-    //         return message.error({
-    //             content: `${translate(lang, 'message_error_recaptcha')}`,
-    //             duration: 3,
-    //             style: {
-    //                 marginTop: '40px'
-    //             }
-    //         });
-    //     }
-    //     const apiURL = `${link}${data}&remoteip=${currentUser.ip}`;
-    //     console.info('apiURL: ', apiURL);
-    //     await axios
-    //         .post(apiURL)
-    //         .then(response => {
-    //             console.info('response: ', response);
-
-    //             if (response.status === 200 && response.data) {
-    //                 dispatch({
-    //                     type: 'ROBOT_CHECK',
-    //                     payload: response.data
-    //                 });
-    //                 message.success({
-    //                     content: `${translate(lang, 'message_success_recaptcha')}`,
-    //                     duration: 3,
-    //                     style: {
-    //                         marginTop: '40px'
-    //                     }
-    //                 });
-    //             }
-    //         })
-    //         .catch(() => {
-    //             message.error({
-    //                 content: `${translate(lang, 'message_error_recaptcha')}`,
-    //                 duration: 3,
-    //                 style: {
-    //                     marginTop: '40px'
-    //                 }
-    //             });
-    //         });
-    // });
+    useEffect(() => {
+        if (ip.length) console.info('::: FX ip: ', ip);
+        if (!currentUser.ip.length) return;
+        if (!ip.length && currentUser.ip.length) setIp(currentUser.ip);
+        console.info('::: FX currentUser: ', currentUser);
+    }, [currentUser, ip]);
 
     const verify = useCallback(async response => {
-        if (!ip.length) setIp(currentUser.ip);
         const captcha = await getRecaptcha(response);
-        console.info('::: verify ip: ', currentUser);
-        // console.info('::: verify captcha.json(): ', captcha.json());
-        console.info('::: verify captcha: ', captcha);
+
+        if (captcha) {
+            dispatch({
+                type: 'ROBOT_CHECK',
+                payload: captcha
+            });
+            message.success({
+                content: `${translate(lang, 'message_success_recaptcha')}`,
+                duration: 3,
+                style: {
+                    marginTop: '40px'
+                }
+            });
+        } else {
+            message.error({
+                content: `${translate(lang, 'message_error_recaptcha')}`,
+                duration: 3,
+                style: {
+                    marginTop: '40px'
+                }
+            });
+        }
     }, []);
 
-    return (
+    return !currentUser.ip ? <LoadingOutlined /> : (
         <Row gutter={24} type="flex" justify="center" align="middle">
             <Col span={12} className="center">
                 <Recaptcha
