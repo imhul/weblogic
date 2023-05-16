05f513414bc227a99f251ec59e54df24802cbcf1
tkachuk <z.tkachuk@nodeart.io>
2023-05-16 21:04:03 +0300

    fix: safe

--- src/hooks/useSafe.js
+++ src/hooks/useSafe.js
@@ -9,18 +9,6 @@ const initial = {
 
 const useSafe = () => {
     const [safe, setSafe] = useState(initial);
-    const [check, setCheck] = useState(false);
-
-    useEffect(() => {
-        const checkProcessEnv = async () => {
-            while (!process.env) {
-                await new Promise(resolve => setTimeout(resolve, 100));
-            }
-            setCheck(true);
-        };
-
-        checkProcessEnv();
-    }, []);
 
     function decode(data) {
         if (!data) {
@@ -32,21 +20,27 @@ const useSafe = () => {
     }
 
     useEffect(() => {
-        if (!check) return;
+        const checkProcessEnv = async () => {
+            while (!process.env) {
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
 
-        setSafe({
-            ...safe,
-            link: decode(process.env.REACT_APP_LINK),
-            key: decode(process.env.REACT_APP_KEY),
-            mCode: decode(process.env.REACT_APP_M_CODE),
-            fCode: decode(process.env.REACT_APP_F_CODE),
-            tCode: decode(process.env.REACT_APP_T_CODE),
-            cv: decode(process.env.REACT_APP_CV),
-            getNF: decode(process.env.REACT_APP_GET_NF),
-            getTG: decode(process.env.REACT_APP_GET_TG),
-            getEmail: decode(process.env.REACT_APP_GET_EMAIL)
-        });
-    }, [check]);
+            setSafe({
+                ...safe,
+                link: decode(process.env.REACT_APP_LINK),
+                key: decode(process.env.REACT_APP_KEY),
+                mCode: decode(process.env.REACT_APP_M_CODE),
+                fCode: decode(process.env.REACT_APP_F_CODE),
+                tCode: decode(process.env.REACT_APP_T_CODE),
+                cv: decode(process.env.REACT_APP_CV),
+                getNF: decode(process.env.REACT_APP_GET_NF),
+                getTG: decode(process.env.REACT_APP_GET_TG),
+                getEmail: decode(process.env.REACT_APP_GET_EMAIL)
+            });
+        };
+
+        checkProcessEnv();
+    }, []);
 
     return safe;
 };
