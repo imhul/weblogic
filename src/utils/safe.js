f71987c5eeb1e47d1413da59523ef4993f38eba2
tkachuk <z.tkachuk@nodeart.io>
2023-05-16 19:04:32 +0300

    netlify forms test 12

--- src/utils/safe.js
+++ src/utils/safe.js
@@ -1,15 +1,19 @@
-import Base64 from './decode';
+// import Base64 from './decode';
+export const decode = data => {
+    let buff = Buffer.from(data, 'base64');
+    return buff.toString('utf8');
+};
 
 const safe = {
-    link: Base64.decode(process.env.REACT_APP_LINK),
-    key: Base64.decode(process.env.REACT_APP_KEY),
-    mCode: Base64.decode(process.env.REACT_APP_M_CODE),
-    fCode: Base64.decode(process.env.REACT_APP_F_CODE),
-    tCode: Base64.decode(process.env.REACT_APP_T_CODE),
-    cv: Base64.decode(process.env.REACT_APP_CV),
-    getNF: Base64.decode(process.env.REACT_APP_GET_NF),
-    getTG: Base64.decode(process.env.REACT_APP_GET_TG),
-    getEmail: Base64.decode(process.env.REACT_APP_GET_EMAIL),
+    link: decode(process.env.REACT_APP_LINK),
+    key: decode(process.env.REACT_APP_KEY),
+    mCode: decode(process.env.REACT_APP_M_CODE),
+    fCode: decode(process.env.REACT_APP_F_CODE),
+    tCode: decode(process.env.REACT_APP_T_CODE),
+    cv: decode(process.env.REACT_APP_CV),
+    getNF: decode(process.env.REACT_APP_GET_NF),
+    getTG: decode(process.env.REACT_APP_GET_TG),
+    getEmail: decode(process.env.REACT_APP_GET_EMAIL),
     ipify: 'https://api.ipify.org/?format=json',
     base: 'https://weblogic.netlify.app',
     vid: 'nETaVY9GOao'
