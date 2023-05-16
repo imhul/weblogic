d9a7f873e613e532e0f8f6edb9effd426a95d896
tkachuk <z.tkachuk@nodeart.io>
2023-05-16 18:12:20 +0300

    format

--- functions/email.js
+++ functions/email.js
@@ -3,60 +3,60 @@ import nodemailer from 'nodemailer';
 import { safe } from './utils/safe';
 
 const build = async (event, context) => {
-  const { mCode, smail, getEmail } = safe;
-  const subject = 'Default subject';
-  const copy = false;
+    const { mCode, smail, getEmail } = safe;
+    const subject = 'Default subject';
+    const copy = false;
 
-  if (true) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        event: event,
-        context: context,
-        body: event.body,
-        queryStringParameters: event.queryStringParameters,
-        rawUrl: event.rawUrl.replace(getEmail + '/?=', '')
-      })
+    if (true) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                event: event,
+                context: context,
+                body: event.body,
+                queryStringParameters: event.queryStringParameters,
+                rawUrl: event.rawUrl.replace(getEmail + '/?=', '')
+            })
+        };
     }
-  }
 
-  try {
-    const { name, email, message } = JSON.parse(event.body);
+    try {
+        const { name, email, message } = JSON.parse(event.body);
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      secure: true,
-      auth: {
-        user: mCode,
-        pass: smail
-      }
-    });
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            secure: true,
+            auth: {
+                user: mCode,
+                pass: smail
+            }
+        });
 
-    const mailOptions = {
-      from: mCode,
-      to: copy ? [mCode, email] : mCode,
-      subject,
-      text: `
+        const mailOptions = {
+            from: mCode,
+            to: copy ? [mCode, email] : mCode,
+            subject,
+            text: `
             Name: ${name}
             Email: ${email}
             Subject: ${subject}
             Message: ${message}
           `
-    };
+        };
 
-    await transporter.sendMail(mailOptions);
+        await transporter.sendMail(mailOptions);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully' })
-    };
-  } catch (error) {
-    console.warn('Error sending email:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error sending email' })
-    };
-  }
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Email sent successfully' })
+        };
+    } catch (error) {
+        console.warn('Error sending email:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error sending email' })
+        };
+    }
 };
 
 const handler = builder(build);
