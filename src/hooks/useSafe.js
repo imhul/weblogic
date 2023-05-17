a906561af68fc6e2cc4a094ac918192a140198e7
tkachuk <z.tkachuk@nodeart.io>
2023-05-17 20:57:58 +0300

    netlify forms test 24

--- src/hooks/useSafe.js
+++ src/hooks/useSafe.js
@@ -19,32 +19,33 @@ const useSafe = () => {
         return buff.toString('utf8');
     }
 
+    function setEnv() {
+        if (!process.env.REACT_APP_LINK) return;
+
+        setSafe({
+            ...initial,
+            link: decode(process.env.REACT_APP_LINK),
+            key: decode(process.env.REACT_APP_KEY),
+            mCode: decode(process.env.REACT_APP_M_CODE),
+            fCode: decode(process.env.REACT_APP_F_CODE),
+            tCode: decode(process.env.REACT_APP_T_CODE),
+            cv: decode(process.env.REACT_APP_CV),
+            getNF: decode(process.env.REACT_APP_GET_NF),
+            getTG: decode(process.env.REACT_APP_GET_TG),
+            getEmail: decode(process.env.REACT_APP_GET_EMAIL)
+        });
+    }
+
     useEffect(() => {
         console.info('::: process.env.REACT_APP_LINK ::: ', process.env.REACT_APP_LINK);
-        console.info('::: process.env ::: ', process.env);
-        console.info('::: process ::: ', process);
-        
-        function setEnv() {
-            setSafe({
-                ...initial,
-                link: decode(process.env.REACT_APP_LINK),
-                key: decode(process.env.REACT_APP_KEY),
-                mCode: decode(process.env.REACT_APP_M_CODE),
-                fCode: decode(process.env.REACT_APP_F_CODE),
-                tCode: decode(process.env.REACT_APP_T_CODE),
-                cv: decode(process.env.REACT_APP_CV),
-                getNF: decode(process.env.REACT_APP_GET_NF),
-                getTG: decode(process.env.REACT_APP_GET_TG),
-                getEmail: decode(process.env.REACT_APP_GET_EMAIL)
-            });
-        }
 
-        if (process.env.REACT_APP_LINK && safe.link) {
-            setEnv();
+        setEnv();
+
+        if (safe.link) {
+            console.info('::: SAFE on start useEffect ::: ', safe);
             return;
         }
 
-        
         const checkProcessEnv = async () => {
             while (!process.env.REACT_APP_LINK) {
                 await new Promise(resolve => setTimeout(resolve, 100));
@@ -52,12 +53,14 @@ const useSafe = () => {
 
             setEnv();
 
-            console.info('::: process.env.REACT_APP_LINK 2 ::: ', safe);
+            console.info('::: SAFE ::: ', safe);
         };
 
         checkProcessEnv();
     }, [process.env, safe]);
 
+    console.info('::: return SAFE ::: ', safe);
+            
     return safe;
 };
 
