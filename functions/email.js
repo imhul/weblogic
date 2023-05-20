1339fcfa24efaa261e61567bdc141472b9957511
tkachuk <z.tkachuk@nodeart.io>
2023-05-20 13:05:57 +0300

    email test 82

--- functions/email.js
+++ functions/email.js
@@ -107,7 +107,7 @@ const build = async event => {
         return {
             statusCode: 523,
             body: JSON.stringify({
-                message: 'Error sending email: ' + (error ?? '::: unknown error :::'),
+                message: 'Error sending email: ' + (error.message ?? error.errorMessage ?? error ?? '::: unknown error :::'),
                 code: 523
             })
         };
