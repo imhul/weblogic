05c71eb434cc3aeca7e2e1120138bb04904638c7
tkachuk <z.tkachuk@nodeart.io>
2023-05-21 21:06:13 +0300

    gpt bot test 3

--- functions/gpt.js
+++ functions/gpt.js
@@ -1,3 +1,4 @@
+import { builder } from '@netlify/functions';
 import { Telegraf, session } from 'telegraf';
 import { message } from 'telegraf/filters';
 import { code } from 'telegraf/format';
@@ -6,67 +7,73 @@ import { env } from './utils/config';
 import ogg from './utils/ogg';
 import ai from './utils/ai';
 
-const INIT_SESSION = { messages: [] };
-const bot = new Telegraf(env.gptBotToken);
-bot.use(session());
+const build = async () => {
+    const INIT_SESSION = { messages: [] };
+    const bot = new Telegraf(env.gptBotToken);
+    bot.use(session());
 
-bot.command('start', async ctx => {
-    ctx.session = INIT_SESSION;
-    ctx.reply('Вітаю!');
-});
+    bot.command('start', async ctx => {
+        ctx.session = INIT_SESSION;
+        ctx.reply('Вітаю!');
+    });
 
-bot.command('new', async ctx => {
-    ctx.session = INIT_SESSION;
-    await ctx.reply('Розпочато нову сессію!');
-});
+    bot.command('new', async ctx => {
+        ctx.session = INIT_SESSION;
+        await ctx.reply('Розпочато нову сессію!');
+    });
 
-bot.on(message('text'), async ctx => {
-    ctx.session ??= INIT_SESSION;
-    ctx.reply(JSON.stringify(ctx.message.text, null, 2)); // as console.log
+    bot.on(message('text'), async ctx => {
+        ctx.session ??= INIT_SESSION;
+        ctx.reply(JSON.stringify(ctx.message.text, null, 2)); // as console.log
 
-    try {
-        await ctx.reply(code('Дай но подумать...'));
-        ctx.session.messages.push({
-            role: ai.roles.USER,
-            content: ctx.message.text
-        });
-        const response = await ai.chat(ctx.session.messages);
-        ctx.session.messages.push({
-            role: ai.roles.ASSISTENT,
-            content: response.content
-        });
-        ctx.reply(response.content);
-    } catch (error) {
-        ctx.reply('Text Bot Error: ' + error.message);
-    }
-});
+        try {
+            await ctx.reply(code('Дай но подумать...'));
+            ctx.session.messages.push({
+                role: ai.roles.USER,
+                content: ctx.message.text
+            });
+            const response = await ai.chat(ctx.session.messages);
+            ctx.session.messages.push({
+                role: ai.roles.ASSISTENT,
+                content: response.content
+            });
+            ctx.reply(response.content);
+        } catch (error) {
+            ctx.reply('Text Bot Error: ' + error.message);
+        }
+    });
 
-bot.on(message('voice'), async ctx => {
-    ctx.session ??= INIT_SESSION;
-    ctx.reply(JSON.stringify(ctx.message.voice, null, 2)); // as console.log
+    bot.on(message('voice'), async ctx => {
+        ctx.session ??= INIT_SESSION;
+        ctx.reply(JSON.stringify(ctx.message.voice, null, 2)); // as console.log
 
-    try {
-        await ctx.reply(code('Дай но подумать...'));
-        const fileLink = await ctx.telegram.getFileLink(
-            ctx.message.voice.file_id
-        );
-        const userID = String(ctx.message.from.id);
-        const oggPath = await ogg.convert(fileLink.href, userID);
-        const mp3path = await ogg.toMp3(oggPath, userID);
-        const text = await ai.voiceReader(mp3path);
-        await ctx.reply(code(text)); // question
-        ctx.session.messages.push({ role: ai.roles.USER, content: text });
-        const response = await ai.chat(ctx.session.messages);
-        ctx.session.messages.push({
-            role: ai.roles.ASSISTENT,
-            content: response.content
-        });
-        ctx.reply(response.content);
-    } catch (error) {
-        ctx.reply('Voice Bot Error: ' + error.message);
-    }
-});
+        try {
+            await ctx.reply(code('Дай но подумать...'));
+            const fileLink = await ctx.telegram.getFileLink(
+                ctx.message.voice.file_id
+            );
+            const userID = String(ctx.message.from.id);
+            const oggPath = await ogg.convert(fileLink.href, userID);
+            const mp3path = await ogg.toMp3(oggPath, userID);
+            const text = await ai.voiceReader(mp3path);
+            await ctx.reply(code(text)); // question
+            ctx.session.messages.push({ role: ai.roles.USER, content: text });
+            const response = await ai.chat(ctx.session.messages);
+            ctx.session.messages.push({
+                role: ai.roles.ASSISTENT,
+                content: response.content
+            });
+            ctx.reply(response.content);
+        } catch (error) {
+            ctx.reply('Voice Bot Error: ' + error.message);
+        }
+    });
 
-bot.launch();
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+    bot.launch();
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+};
+
+const handler = builder(build);
+
+export { handler };
