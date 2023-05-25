2220403a94a6d364220f001d04eee7d184d47007
tkachuk <z.tkachuk@nodeart.io>
2023-05-25 22:27:01 +0300

    mongo test 35

--- functions/mongo.js
+++ functions/mongo.js
@@ -27,14 +27,14 @@ const build = async event => {
         const db = client.db(data.db); // .command({ ping: 1 });
         const collection = db.collection(data.collection);
         console.info('data.query: ', data.query);
-        const result = collection.find(data.query);
-        console.info('result: ', result.toArray());
+        const result = collection.find(data.query).toArray();
+        console.info('result: ', result);
         if (result.length) {
             return {
                 statusCode: 200, body: JSON.stringify({
                     ok: true,
                     code: 200,
-                    data: result.toArray()
+                    data: result
                 })
             };
         } else {
