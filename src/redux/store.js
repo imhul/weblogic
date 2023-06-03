12a64e13f41908765cac52e25b533eae77058f53
tkachuk <z.tkachuk@nodeart.io>
2023-06-03 19:49:56 +0300

    todos update

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ const store = configureStore({
     reducer: reducer,
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(thunk),
-    devTools: true, // process.env.NODE_ENV !== 'production',
+    devTools: true, // TODO: process.env.NODE_ENV !== 'production',
     preloadedState: {}
 });
 
