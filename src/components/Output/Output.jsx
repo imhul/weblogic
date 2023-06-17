7483acee0619abc4e0ac8938b4a33dd78a7a8c84
tkachuk <z.tkachuk@nodeart.io>
2023-06-17 17:13:46 +0300

    auth test 87

--- src/components/Output/Output.jsx
+++ src/components/Output/Output.jsx
@@ -24,7 +24,7 @@ import Notify from '../Notify';
 // hooks
 import useInitialization from '../../hooks/useInitialization';
 import useSafe from '../../hooks/useSafe';
-import useIpify from '../../hooks/useIpify';
+// import useIpify from '../../hooks/useIpify';
 import useLang from '../../hooks/useLang';
 import useAllUsers from '../../hooks/useAllUsers';
 import useCookies from '../../hooks/useCookies';
@@ -51,7 +51,7 @@ const Output = () => {
     useInitialization();
     useSafe();
     const { safe, location } = useSelector(s => s.ui);
-    useIpify(safe);
+    // useIpify(safe);
     useLang();
     // useTip();
     useAllUsers();
